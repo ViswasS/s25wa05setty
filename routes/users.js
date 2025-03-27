e78ed369839402d6f47aaee21070389ae84eb333
Viswas <s.viswas111@gmail.com>
2025-03-26 20:49:18 -0500

    changes are made

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+let ratio = 0.75;
+let acc = 1000;
+
+router.get('/', (req, res) => {
+    acc = acc * ratio; // Multiply acc by ratio
+    res.send(`Value is: ${acc}`); // Display the updated acc value
 });
 
 module.exports = router;
