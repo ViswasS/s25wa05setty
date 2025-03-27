37cb2d22f101a81c01508d21bc0ca876031f0824
Viswas <s.viswas111@gmail.com>
2025-03-26 20:45:40 -0500

    new changes

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Viswas Shetty' });
+  res.render('mydata', { title: 'Viswas Shetty' });
 });
 
 module.exports = router;
